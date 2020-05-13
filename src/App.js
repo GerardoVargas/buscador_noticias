1616ebc54d49f98a03fb67e703254030edd9840f
Gerardo_Vargas <evc414@gmail.com>
2020-05-13 03:15:44 -0500

    request API Data & save results in the state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 
@@ -6,7 +6,20 @@ function App() {
 
   //definir categoria y noticias
   const [categoria, guardarCategoria] = useState('')
+  const [noticias, guardarNoticias] = useState([])
 
+  //consultar la API
+  useEffect( () => {
+      const consultarAPI = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=499a5c1557ce4d2ea761e7ebe8f8a4c1`
+    
+        const respuesta = await fetch(url)
+        const noticias = await respuesta.json()
+        guardarNoticias(noticias.articles)
+      }
+    consultarAPI()
+
+  }, [categoria])
 
   return (
     <Fragment>
