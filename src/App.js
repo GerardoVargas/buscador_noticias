0a36389262c6914f37f9c7a84b23f0a7aa978c06
Gerardo_Vargas <evc414@gmail.com>
2020-05-13 02:32:19 -0500

    Formulario Component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {Fragment} from 'react';
 import Header from './components/Header'
+import Formulario from './components/Formulario'
 
 function App() {
   return (
     <Fragment>
       <Header titulo="Buscador de Noticias"/>
+
+      <Formulario/>
+      
     </Fragment>
   );
 }
